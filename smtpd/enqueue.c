53d36456f16ff15aecf835de122cd7c71f98a879
Todd T. Fries <todd@fries.net>
2012-08-22 11:20:13 -0500

    Ignorance is bliss, so ignore -R return and deliver the message..

--- smtpd/enqueue.c
+++ smtpd/enqueue.c
@@ -175,7 +175,7 @@ enqueue(int argc, char *argv[])
 	time(&timestamp);
 
 	while ((ch = getopt(argc, argv,
-	    "A:B:b:E::e:F:f:iJ::L:mo:p:qtvx")) != -1) {
+	    "A:B:b:E::e:F:f:iJ::L:mo:p:qR:tvx")) != -1) {
 		switch (ch) {
 		case 'f':
 			fake_from = optarg;
@@ -200,6 +200,7 @@ enqueue(int argc, char *argv[])
 		case 'm':
 		case 'o':
 		case 'p':
+		case 'R':
 		case 'x':
 			break;
 		case 'q':
