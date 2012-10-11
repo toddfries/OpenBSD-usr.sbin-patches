ccbdf00f076bad7e4a95cc38647a2ba7134d02f5
Todd T. Fries <todd@fries.net>
2012-10-11 16:29:03 -0500

    user filter

--- smtpd/filters_examples/userfilter/filter_user.c
+++ smtpd/filters_examples/userfilter/filter_user.c
@@ -22,10 +22,6 @@ int match_domain(char *, char *);
 int match_rcpt(char *, char *);
 enum filter_status rcpt_cb(uint64_t, struct filter_rcpt *, void *);
 
-struct filter_user_arg
-{
-	sqlite3 *db;
-};
 struct filter_user_sql_arg {
 	int ok;
 	char *email;
@@ -33,13 +29,14 @@ struct filter_user_sql_arg {
 int
 match_domain(char *rcpt, char *domain)
 {
-	char *rcptdomain;
+	char *rcptdomain, *tmp;
+	tmp = strdup(rcpt);
 
-	rcptdomain = strchr(rcpt, '@');
-	if (rcptdomain++ == NULL)
+	rcptdomain = strchr(tmp, '@');
+	if (rcptdomain == NULL)
 		return 0;
 
-	if (strncasecmp(rcptdomain, domain, strlen(domain)) == 0)
+	if (strncasecmp(rcptdomain+1, domain, strlen(domain)) == 0)
 		return 1;
 
 	return 0;
@@ -59,14 +56,14 @@ sql_domain_callback( void *arg, int argc, char **argv, char **colname )
 {
 	int i;
 	struct filter_user_sql_arg *user_sql_arg = arg;
-	log_info("sql_domain_callback: starting email %s", user_sql_arg->email);
+	log_debug("sql_domain_callback: starting email %s", user_sql_arg->email);
 	for (i=0; i<argc; i++) {
 		if (strncmp(DOMAIN_COL_NAME, colname[i],
 		    strlen(DOMAIN_COL_NAME)) == 0) {
 			log_debug("%d: Comparing db user %s with email %s", i,
 			    argv[i], user_sql_arg->email);
 			if (match_domain(user_sql_arg->email, argv[i])) {
-				log_info("match!");
+				log_info("matched domain %s!", argv[i]);
 				user_sql_arg->ok = 1;
 				break;
 			}
@@ -89,7 +86,7 @@ sql_user_callback( void *arg, int argc, char **argv, char **colname )
 			log_debug("%d: Comparing db user %s with email %s", i,
 			    argv[i], user_sql_arg->email);
 			if (match_rcpt(user_sql_arg->email, argv[i])) {
-				log_info("match!");
+				log_info("matched user %s!", argv[i]);
 				user_sql_arg->ok = 1;
 				break;
 			}
@@ -103,10 +100,10 @@ sql_user_callback( void *arg, int argc, char **argv, char **colname )
 enum filter_status
 rcpt_cb(uint64_t id, struct filter_rcpt *rcpt, void *p)
 {
-	struct filter_user_arg	*fda = p;
 	char *errmsg = NULL;
 	int rc;
 	struct filter_user_sql_arg user_sql_arg;
+	sqlite3 *db;
 
 	user_sql_arg.ok = 0;
 
@@ -114,6 +111,12 @@ rcpt_cb(uint64_t id, struct filter_rcpt *rcpt, void *p)
 
 	log_info("[%llx] RCPT: %s", id, user_sql_arg.email);
 
+	rc = sqlite3_open("/etc/mail/users.db", &db);
+	if ( rc ) {
+		log_info("Can't open database: %s", sqlite3_errmsg(db));
+		/* XXX is this right? */
+		return STATUS_ACCEPT;
+	}
 
 	/*
 	 * check if the rcpt falls in a domain we need to check users for,
@@ -121,7 +124,7 @@ rcpt_cb(uint64_t id, struct filter_rcpt *rcpt, void *p)
 	 * mail etc
 	 *
 	 */
-	rc = sqlite3_exec(fda->db, "select * from " DOMAIN_TAB_NAME,
+	rc = sqlite3_exec(db, "select * from " DOMAIN_TAB_NAME,
 	    sql_domain_callback, (void *)&user_sql_arg, &errmsg);
 	if (rc != SQLITE_OK) {
 		log_info("SQL error: %s", errmsg);
@@ -132,7 +135,7 @@ rcpt_cb(uint64_t id, struct filter_rcpt *rcpt, void *p)
 
 	user_sql_arg.ok = 0;
 
-	rc = sqlite3_exec(fda->db, "select * from " USER_TAB_NAME,
+	rc = sqlite3_exec(db, "select * from " USER_TAB_NAME,
 	    sql_user_callback, (void *)&user_sql_arg, &errmsg);
 	if (rc != SQLITE_OK) {
 		log_info("SQL error: %s", errmsg);
@@ -143,38 +146,24 @@ rcpt_cb(uint64_t id, struct filter_rcpt *rcpt, void *p)
 	if (user_sql_arg.ok == 0)
 		return STATUS_REJECT;
 
+	sqlite3_close(db);
+
 	return STATUS_ACCEPT;
 }
 
 int
 main(int argc, char *argv[])
 {
-	struct filter_user_arg	*fda;
-	int rc;
-
 	log_init(0);
 	log_verbose(1);
-
-	if ((fda = calloc(1, sizeof(struct filter_user_arg))) == NULL)
-		errx(EXIT_FAILURE, "calloc");
-
-	rc = sqlite3_open("/etc/mail/users.db", &fda->db);
-	if ( rc ) {
-		log_info("Can't open database: %s", sqlite3_errmsg(fda->db));
-		exit(1);
-	}
 
 	log_info("Starting filter!");
 
 	filter_init();
-	filter_register_rcpt_callback(rcpt_cb, fda);
+	filter_register_rcpt_callback(rcpt_cb, NULL);
 
 	log_info("Starting filter_loop!");
 	filter_loop();
-
-	free(fda);
-
-	sqlite3_close(fda->db);
 
 	return EXIT_SUCCESS;
 }
